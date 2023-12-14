76715a9550f8e0df59a68d0c17662bc39378aaee
David Muñoz <dmp.david@gmail.com>
2023-12-14 22:50:40 +0100

    fin sección 28

---
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../app.routes';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [ RouterModule ],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() {
+  // const dashboardRoutes = routes
+  //   .map( route => route.children ?? [] )
+  //   .flat()
+  //   .filter( route => route && route.path )
+  //   .filter( route => !route.path?.includes(':') )
+  // console.log(dashboardRoutes);
+  }
+}
